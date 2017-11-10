00082012408a52862526bf7fb88adecbf41091b1
Arthur Sarkisyan <arthurunique24@gmail.com>
2017-11-11 01:57:41 +0300

    Pagination, not completed yet

--- Web/HW3/ask_sarkisyan/questions/views.py
+++ Web/HW3/ask_sarkisyan/questions/views.py
@@ -1,39 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-QUESTIONS = {
-    '1': {'id': 1, 'title': 'I`m your dream', 'text': 'I`m your dream, make you real'},
-    '2': {'id': 2, 'title': 'I`m your eyes', 'text': 'I`m your eyes when you must steal'},
-    '3': {'id': 3, 'title': 'I`m your pain', 'text': 'I`m your pain when you can`t feel'},
-}
+
+def init():
+    questions = []
+    for i in range(0, 100):
+        questions.append({
+            'title': 'Title in question ' + str(i),
+            'id': i,
+            'text': 'Text in question ' + str(i),
+            'likes': 5,
+            'tags': ['Java', 'Android', 'SQL'],
+            'isVote': 0,
+            'count_answers': 31,
+            'answers': [{'text': 'answer1', 'isCorrect': 1, 'likes': 5},
+                        {'text': 'answer2', 'isCorrect': 0, 'likes': 5}],
+        })
+    hotTags = ['Python', 'C++', 'Android']
+    # bestMembers = ['Mr.Freeman', 'Dr.House', 'Bender', 'Homer', 'Rick']
+    Authorized = 1
+    return {'questions': questions, 'hotTags': hotTags, 'Authorized': Authorized}
+
+
+def pagination(request):
+    init_list = init()
+    paginator = Paginator(init_list['questions'], 3)
+    page = request.GET.get('page')
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+    return articles
 
 
 def index(request):
-    return render(request, 'index.html', {'questions': QUESTIONS.values()})
+    init_list = init()
+    return render_to_response('index.html', {'isAuthorized': init_list['Authorized'],
+                                             'questions': pagination(request),
+                                             'tags': init_list['hotTags']})
 
 
 def ask(request):
-    return render(request, 'ask.html', {'questions': QUESTIONS.values()})
-
-
-def login(request):
-    return render(request, 'login.html', {'questions': QUESTIONS.values()})
+    init_list = init()
+    return render_to_response('ask.html', {'isAuthorized': init_list['Authorized'],
+                                           'questions': pagination(request),
+                                           'tags': init_list['hotTags']})
 
 
 def question(request):
-    return render(request, 'question.html', {'questions': QUESTIONS.values()})
+    init_list = init()
+    return render_to_response('question.html', {'isAuthorized': init_list['Authorized'],
+                                                'question': init_list['questions'],
+                                                'tags': init_list['hotTags']})
+
+
+def login(request):
+    init_list = init()
+    return render_to_response('login.html', {'isAuthorized': init_list['Authorized'],
+                                             'questions': pagination(request),
+                                             'tags': init_list['hotTags']})
+
 
 
 def settings(request):
-    return render(request, 'settings.html', {'questions': QUESTIONS.values()})
+    init_list = init()
+    return render_to_response('settings.html', {'isAuthorized': init_list['Authorized'],
+                                                'questions': pagination(request),
+                                                'tags': init_list['hotTags']})
 
 
 def signup(request):
-    return render(request, 'signup.html', {'questions': QUESTIONS.values()})
+    init_list = init()
+    return render_to_response('signup.html', {'isAuthorized': init_list['Authorized'],
+                                              'questions': pagination(request),
+                                              'tags': init_list['hotTags']})
 
 
 def hot(request):
-    return render(request, 'hotQuestions.html', {'questions': QUESTIONS.values()})
+    init_list = init()
+    return render_to_response('hotQuestions.html', {'isAuthorized': init_list['Authorized'],
+                                                    'questions': pagination(request),
+                                                    'tags': init_list['hotTags']})
 
 
 def questionsbytag(request):
-    return render(request, 'questionsbytag.html', {'questions': QUESTIONS.values()})
+    init_list = init()
+    return render_to_response('questionsbytag.html', {'isAuthorized': init_list['Authorized'],
+                                                      'questions': pagination(request),
+                                                      'tags': init_list['hotTags']})
